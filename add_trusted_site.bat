8d100fa6a5e6fe7022530768d77dd550fb520edb
BB-25004 <watcharapol@bright-better.com>
2026-01-20 17:16:45 +0700

    update: Trusted Site comment

--- add_trusted_site.bat
+++ add_trusted_site.bat
@@ -1,6 +1,17 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+REM =========================================================
+REM Windows Internet Options - Security Zones
+REM ---------------------------------------------------------
+REM Zone 0 = My Computer        (Local machine)
+REM Zone 1 = Local Intranet    (Internal network)
+REM Zone 2 = Trusted Sites     (Less restricted, trusted web)
+REM Zone 3 = Internet          (Default internet zone)
+REM Zone 4 = Restricted Sites (High security)
+REM =========================================================
+
+
 REM ==============================
 REM Validate argument
 REM ==============================
@@ -27,6 +38,7 @@ for /f "tokens=1 delims=/" %%A in ("%URL_NO_PROTO%") do (
 
 REM ==============================
 REM Add Trusted Site (Zone 2)
+REM Zone 2 = Trusted Sites
 REM ==============================
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains\!DOMAIN!" /f >nul
 
