1cdc64986ece690181adc41246b515312f0f5b58
BB-25004 <watcharapol@bright-better.com>
2026-02-03 15:01:19 +0700

    refactor: client js (Input Sales Order screen is shown Data from Quotation is referred Ship To and Bill To is default as Customer)

--- modules/press/Sale Order module/Create Sales Order/jsClient/client_main.js
+++ modules/press/Sale Order module/Create Sales Order/jsClient/client_main.js
@@ -1,24 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-        let obj = document.getElementById('JKN_AREA');
-        if (obj) obj.style.display = 'none';
-
-
-        // let QtNo = document.getElementsByName('CNDTN_I_QT_NO')[0];
-        // if (QtNo) document.getElementById('TLN_1_I_QT_NO').value = QtNo.value;
-
-        // let SoNo = document.getElementsByName('CNDTN_I_SONO')[0];
-        // if (SoNo) document.getElementById('TLN_1_I_SONO').value = SoNo.value;
-
-        let Name = document.getElementsByName('CNDTN_I_NAME')[0];
-        if (Name) document.getElementById('TLN_1_I_NAME').value = Name.value;
-
-    }, 200);
-});
-
-function resizeContents_end() {
-    let obj = document.getElementById('JKN_AREA');
-    if (obj) obj.style.display = 'none';
+  setTimeout(function () {
+    let obj = document.getElementById("JKN_AREA");
+    if (obj) obj.style.display = "none";
 
     // let QtNo = document.getElementsByName('CNDTN_I_QT_NO')[0];
     // if (QtNo) document.getElementById('TLN_1_I_QT_NO').value = QtNo.value;
@@ -26,57 +9,83 @@ function resizeContents_end() {
     // let SoNo = document.getElementsByName('CNDTN_I_SONO')[0];
     // if (SoNo) document.getElementById('TLN_1_I_SONO').value = SoNo.value;
 
-    let Name = document.getElementsByName('CNDTN_I_NAME')[0];
-    if (Name) document.getElementById('TLN_1_I_NAME').value = Name.value;
+    let Name = document.getElementsByName("CNDTN_I_NAME")[0];
+    if (Name) document.getElementById("TLN_1_I_NAME").value = Name.value;
+  }, 200);
+});
 
-    function updateBoxHeight() {
-        function removeHeight(id) {
-            const el = document.getElementById(id);
-            if (el) el.style.removeProperty("height");
-        }
+function updateBoxHeight() {
+  function removeHeight(id) {
+    const el = document.getElementById(id);
+    if (el) el.style.removeProperty("height");
+  }
 
-        // Box Card 0 พิเศษ
-        removeHeight("BASE:0:block");
+  // Box Card 0 พิเศษ
+  removeHeight("BASE:0:block");
 
-        const baseCard = document.getElementById("BASE:0:CARD");
-        if (baseCard) {
-            baseCard.setAttribute("style", "overflow: auto;");
-        }
+  const baseCard = document.getElementById("BASE:0:CARD");
+  if (baseCard) {
+    baseCard.setAttribute("style", "overflow: auto;");
+  }
 
-        for (let i = 1; i <= 2; i++) {
-            removeHeight(`box${i}`);
-            removeHeight(`BASE:${i}:block`);
-            removeHeight(`tbl_scrollable_body_L_${i}`);
-        }
-    }
-
-    updateBoxHeight();
-
-    function setRowNumbers(selectorPrefix) {
-        const rowNoBoxes = document.querySelectorAll(`input[id^="${selectorPrefix}"]`);
-        if (rowNoBoxes.length > 0) {
-            let rowIndex = 1;
-            rowNoBoxes.forEach((input) => {
-                if (!input.value.trim()) {
-                    input.value = rowIndex.toString();
-                }
-                rowIndex++;
-            });
-        }
-    }
-
-    setRowNumbers("TLN_2_I_LNNO_");
-    
-    
+  for (let i = 1; i <= 2; i++) {
+    removeHeight(`box${i}`);
+    removeHeight(`BASE:${i}:block`);
+    removeHeight(`tbl_scrollable_body_L_${i}`);
+  }
 }
 
-function addComma(elem){
-    let val = (elem && elem.value !== undefined) ? elem.value : elem;
+function setRowNumbers(selectorPrefix) {
+  const rowNoBoxes = document.querySelectorAll(
+    `input[id^="${selectorPrefix}"]`,
+  );
+  if (rowNoBoxes.length > 0) {
+    let rowIndex = 1;
+    rowNoBoxes.forEach((input) => {
+      if (!input.value.trim()) {
+        input.value = rowIndex.toString();
+      }
+      rowIndex++;
+    });
+  }
+}
 
-    val = String(val).replace(/[^\d\.]/g, '');
+function lookup() {
+  let _I_CSCODE = document.getElementById("TLN_1_I_CSCODE").value;
 
-    let num = Number(val);
-    if (isNaN(num)) num = 0;
+  document.getElementById("TLN_1_I_SHIPTO").value = _I_CSCODE;
+  document.getElementById("TLN_1_I_BILLTO").value = _I_CSCODE;
+}
 
-    return num.toFixed(2).replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+function resizeContents_end() {
+  let obj = document.getElementById("JKN_AREA");
+  if (obj) obj.style.display = "none";
+
+  // let QtNo = document.getElementsByName('CNDTN_I_QT_NO')[0];
+  // if (QtNo) document.getElementById('TLN_1_I_QT_NO').value = QtNo.value;
+
+  // let SoNo = document.getElementsByName('CNDTN_I_SONO')[0];
+  // if (SoNo) document.getElementById('TLN_1_I_SONO').value = SoNo.value;
+
+  let Name = document.getElementsByName("CNDTN_I_NAME")[0];
+  if (Name) document.getElementById("TLN_1_I_NAME").value = Name.value;
+
+  let _I_CSCODE = document.getElementById("TLN_1_I_CSCODE").value;
+  let _I_SHIPTO = document.getElementById("TLN_1_I_SHIPTO").value;
+  let _I_BILLTO = document.getElementById("TLN_1_I_BILLTO").value;
+
+  updateBoxHeight();
+  setRowNumbers("TLN_2_I_LNNO_");
+  lookup();
+}
+
+function addComma(elem) {
+  let val = elem && elem.value !== undefined ? elem.value : elem;
+
+  val = String(val).replace(/[^\d\.]/g, "");
+
+  let num = Number(val);
+  if (isNaN(num)) num = 0;
+
+  return num.toFixed(2).replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 }
